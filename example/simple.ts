996b306eeff4fb2f4f5fac3585f962562cd806ee
WMXPY <wm@mengw.io>
2020-01-31 12:27:57 -0600

    implement init io example

--- example/simple.ts
+++ example/simple.ts
@@ -5,6 +5,8 @@
  */
 
 import { SudooExpress, SudooExpressApplication } from "@sudoo/express";
+import { Request, Response } from "express";
+import * as Path from "path";
 import * as SocketIO from "socket.io";
 
 const setting: SudooExpressApplication = SudooExpressApplication.create('Bark Shell - Example', "1.0.0");
@@ -12,8 +14,20 @@ const app: SudooExpress = SudooExpress.create(setting);
 
 const io: SocketIO.Server = SocketIO(app.http);
 
+app.express.get('/', (req: Request, res: Response) => {
+    res.sendFile(Path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 io.on('connection', (socket: SocketIO.Socket) => {
     console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg: string) => {
+        console.log(msg);
+    });
 });
 
 // tslint:disable-next-line: no-magic-numbers
