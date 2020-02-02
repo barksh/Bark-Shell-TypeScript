31be6847323029222338c981cd6ba20d5529a41e
WMXPY <wm@mengw.io>
2020-02-01 18:34:13 -0600

    add context config

--- src/status/user.ts
+++ src/status/user.ts
@@ -4,25 +4,39 @@
  * @description User
  */
 
-export class BarkUser<T extends string = string> {
+export class BarkUser<A extends any = string, S extends string = string> {
 
-    public static create<T extends string = string>(username: string, initialStatus?: T): BarkUser<T> {
+    public static create<A extends any = string, S extends string = string>(authorization: A, initialStatus?: S): BarkUser<A, S> {
 
-        return new BarkUser<T>(username, initialStatus);
+        const statusStack: S[] = initialStatus ? [initialStatus] : [];
+        const context: Map<string, any> = new Map();
+        return new BarkUser<A, S>(authorization, statusStack, context);
     }
 
-    private readonly _username: string;
+    public static restore<A extends any = string, S extends string = string>(authorization: A, statusStack: S[], context: Record<string, any>): BarkUser<A, S> {
 
-    private readonly _statusStack: T[];
-
-    private constructor(username: string, initialStatus?: T) {
-
-        this._username = username;
-        this._statusStack = initialStatus ? [initialStatus] : [];
+        const contextMap: Map<string, any> = new Map();
+        const keys: string[] = Object.keys(context);
+        for (const key of keys) {
+            contextMap.set(key, context[key]);
+        }
+        return new BarkUser<A, S>(authorization, statusStack, contextMap);
     }
 
-    public get username(): string {
-        return this._username;
+    private readonly _authorization: A;
+
+    private readonly _statusStack: S[];
+    private readonly _context: Map<string, any>;
+
+    private constructor(authorization: A, statusStack: S[], context: Map<string, any>) {
+
+        this._authorization = authorization;
+        this._statusStack = statusStack;
+        this._context = context;
+    }
+
+    public get authorization(): A {
+        return this._authorization;
     }
     public get length(): number {
         return this._statusStack.length;
@@ -30,22 +44,22 @@ export class BarkUser<T extends string = string> {
     public get isEmptyStatus(): boolean {
         return this._statusStack.length === 0;
     }
-    public get currentStatus(): T | undefined {
+    public get currentStatus(): S | undefined {
         return this._statusStack[0];
     }
-    public get statusStack(): T[] {
+    public get statusStack(): S[] {
         return this._statusStack;
     }
 
-    public pushStatus(status: T): this {
+    public pushStatus(status: S): this {
 
         this._statusStack.unshift(status);
         return this;
     }
 
-    public popStatus(): T | undefined {
+    public popStatus(): S | undefined {
 
-        const currentStatus: T | undefined = this.statusStack.shift();
+        const currentStatus: S | undefined = this.statusStack.shift();
         return currentStatus;
     }
 }
